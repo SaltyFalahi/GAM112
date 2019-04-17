c547378fa8ff97d13666ff4a0681dcd42df25270
Christos257 <christos257@gmail.com>
2019-04-17 03:08:10 -0700

    Shooting script works.

--- Assets/Scripts/Player/PlayerShooting.cs
+++ Assets/Scripts/Player/PlayerShooting.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class PlayerShooting : MonoBehaviour
 {
     Rigidbody2D rb2d;
-    Vector3 mousePos;
     public GameObject bullet;
     public GameObject gun;
     public float speed;
@@ -21,24 +20,15 @@ public class PlayerShooting : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Vector2 point = Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
+            Vector2 point = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             Vector2 position = new Vector2(gun.transform.position.x, gun.transform.position.y);
             Vector2 direction = point - position;
             direction.Normalize();
-            GameObject projectile = (GameObject)Instantiate(bullet, point, Quaternion.identity);
+            GameObject projectile = (GameObject)Instantiate(bullet, position, Quaternion.identity);
             projectile.GetComponent<Rigidbody2D>().velocity = direction * speed;
+            projectile.transform.up = direction;
             Debug.Log(point);
+            Debug.Log(position);
         }
     }
-
-    /*public void PlayerShootPosition()
-    {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        if (Physics2D.Raycast(ray, out hit, 1000))
-        {
-            mousePos = hit.point;
-            Debug.Log("LMAO");
-        }
-    }*/
 }
